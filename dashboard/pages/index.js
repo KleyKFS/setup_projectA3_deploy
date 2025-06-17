54e123b908c5aff76caee2e7d4f22c102c609ee1
KleyKFS <160678780+KleyKFS@users.noreply.github.com>
2025-06-17 18:03:18 -0300

    primeiro commit

---
+++ dashboard/pages/index.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export default function Dashboard() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        const socket = new WebSocket("wss://setup-projecta3-deploy.onrender.com/ws");
+        socket.onmessage = (event) => setMessage(event.data);
+
+        return () => socket.close();
+ }, []);
+
+ return <div>Mensagem em tempo real: {message}</div>;
+}
